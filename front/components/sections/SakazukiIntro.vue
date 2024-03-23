1917727e1ae560806c78b3bd62152466c14bc1b5
pdmuds4 <s2222015@stu.musashino-u.ac.jp>
2024-03-23 16:06:37 +0900

    導入メリットができました

--- front/components/sections/SakazukiIntro.vue
+++ front/components/sections/SakazukiIntro.vue
@@ -1,5 +1,79 @@
 <template>
-about
+    <div class="melit-body">
+        <h1 class="melit-title">導入メリット</h1>
+        
+        <!-- melit 1 -->
+        <div class="melit-content">
+            <div class="melit-block melit-left">
+                <img 
+                    class = "melit-image"
+                    src="https://cdn.discordapp.com/attachments/1219948957048770610/1220979552155729941/2024-03-23_15.16.34.png?ex=6610e8d1&is=65fe73d1&hm=ec7c134ea967bdb9560daf81ac2c7582301d5f3f7a8999115c7ae41c7cf00c9d&">
+            </div>
+            <div class="melit-block melit-right">
+                <div class="block-title">
+                    <span>1.</span>
+                    <div>
+                        <h1 class="title-row1">メンバーのキャリアを</h1>
+                        <h1 class="title-row1">証明書として可視化できる</h1>
+                    </div>
+                </div>
+                <div class="block-text">
+                    <h2>
+                        メンバーの成長を「キャリア証明書」で管理することで、
+                        <br>デジタル上で検証可能な個人情報として扱えます。
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- melit 2 -->
+        <div class="melit-content">
+            <div class="melit-block melit-right">
+                <div class="block-title">
+                    <span>2.</span>
+                    <div>
+                        <h1 class="title-row1">証明書で、企業からの</h1>
+                        <h1 class="title-row1">オファーを受け取れる</h1>
+                    </div>
+                </div>
+                <div class="block-text">
+                    <h2>
+                        キャリア証明書をスカウトプラットフォームに連携すると、
+                        <br>さまざまな企業から優良なオファーが届きます。
+                    </h2>
+                </div>
+            </div>
+            <div class="melit-block melit-left">
+                <img 
+                    class = "melit-image"
+                    src="https://cdn.discordapp.com/attachments/1219948957048770610/1220989420988137504/2024-03-23_15.55.40.png?ex=6610f202&is=65fe7d02&hm=1ab9943d3d2f296afe735de2069566f7474b82f6b53340b7643874d616fdeaaf&">
+            </div>
+        </div>
+
+        <!-- melit 3 -->
+        <div class="melit-content">
+            <div class="melit-block melit-left">
+                <img 
+                    class = "melit-image"
+                    src="https://media.canva.com/1/image-resize/1/898_732_100_PNG_F/czM6Ly9tZWRpYS1wcml2YXRlLmNhbnZhLmNvbS9ieFc2SS9NQUY2VE5ieFc2SS8xL3AucG5n?osig=AAAAAAAAAAAAAAAAAAAAABsQpJJIpF_bnKH0ojK9osGOdOKWt2rIacdEh85xWhS1&exp=1711193959&x-canva-quality=screen_2x&csig=AAAAAAAAAAAAAAAAAAAAAEasn-JAQgELM4bVWxsDn5p-X8snwEzanvy68lr9O3kr">
+            </div>
+            <div class="melit-block melit-right">
+                <div class="block-title">
+                    <span>3.</span>
+                    <div>
+                        <h1 class="title-row1">キャリアパス推薦から</h1>
+                        <h1 class="title-row1">キャリア計画をスムーズに</h1>
+                    </div>
+                </div>
+                <div class="block-text">
+                    <h2>
+                        独自のダッシュボードで、「所属大学」「インターン先」
+                        <br>「就職先」までの一連のキャリアを可視化できます。
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -7,4 +81,62 @@ about
 </script>
 
 <style>
+.melit-body {
+    padding: 25px;
+    background-color: #7FC0EA;
+    color: #1D4592;
+}
+
+.melit-title {
+    text-align: center;
+    font-size: 64px;
+}
+
+.melit-content {
+    display: flex;
+}
+
+.melit-block {
+    width: 50%;
+    padding: 80px 50px;
+}
+
+.melit-left {
+    text-align: center;
+}
+
+.melit-image {
+    width: 100%;
+    height: 500px;
+    border-radius: 30px;
+    box-shadow: 10px 10px 0px #1D4592;
+    object-fit: cover;
+}
+
+.block-title {
+    display: flex;
+}
+
+.block-title span {
+    margin: 0;
+    font-size: 100px;
+    font-weight: bold;
+}
+
+.block-title div {
+    margin-top: 10px;
+    margin-left: 30px;
+}
+
+.title-row1 {
+    font-size: 40px;
+    margin: 0;
+}
+
+.block-text {
+    margin-top: 50px;
+    margin-left: 10px;
+    line-height: 50px;
+}
+
 </style>
