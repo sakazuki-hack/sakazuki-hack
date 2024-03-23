0899639c27f38307c3c0c1ecbc92f14883f0fb27
Akane Yoshii <s2222059@stu.musashino-u.ac.jp>
2024-03-23 16:04:20 +0900

    header,footerを作成しました。

--- front/components/base/Footer.vue
+++ front/components/base/Footer.vue
@@ -1,5 +1,25 @@
 <template>
-footer
+    <div class="section"></div>
+    <footer class="footer">
+        <div class="footer-content">
+            <div class="footer-content-right">
+                <div class="footer-logo">
+                    <a href="#"><img src="https://cdn.discordapp.com/attachments/1219948957048770610/1220980066218016838/sakazuki_logo_box_black_rgb.png?ex=6610e94c&is=65fe744c&hm=e6d83babe51a5304aba2feb16e8e2d8113aad08fac94d36b190ee234199c0187&" alt="Twitter"></a>
+                </div>
+            </div>
+                <div class="footer-links">
+                    <ul>
+                        <li class="footer-item"><a href="#conpany">運営会社</a></li>
+                        <li class="footer-item"><a href="#recrut">採用情報</a></li>
+                        <li class="footer-item"><a href="#promiss">利用規約</a></li>
+                        <li class="footer-item"><a href="#policy">プライバシーポリシー</a></li>
+                    </ul>
+                </div>
+            </div>
+        <div class="footer-legal">
+            <p>©PitPa, Inc.</p>
+        </div>
+    </footer>
 </template>
 
 <script>
@@ -7,4 +27,76 @@ footer
 </script>
 
 <style>
+.section{
+    /* width: 100px; */
+    height: 500px;
+}
+.footer {
+    padding: 10px;
+    background: rgb(51, 51, 51);
+}
+
+.footer-content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    margin: 20px;
+}
+.footer-content-right{
+    display: flex;
+    align-items: center;
+}
+.footer-logo {
+    width: auto;
+}
+
+.footer-logo img {
+    width: 130px;
+    /* max-width: 100px;  */
+}
+
+.footer-links ul {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+    display: flex;
+}
+.footer-links a{
+    color: rgb(255, 255, 255);
+    text-decoration: none;
+}
+.footer-item {
+    position: relative;
+    padding: 0 20px;
+    font-size: 13px;
+    
+}
+
+.footer-item:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    right: 0;
+    top: 0;
+    bottom: 0;
+    width: 1px;
+    background-color: #ffffff;
+}
+
+.social-icons a {
+    margin: 8px;
+    display: inline-block;
+}
+
+.social-icons img {
+    width: 30px;
+    height: auto;
+}
+
+.footer-legal p {
+    color: #888;
+    font-size: 12px;
+    margin-left: 20px;
+}
+
+
 </style>
--- front/components/base/Header.vue
+++ front/components/base/Header.vue
@@ -1,10 +1,246 @@
 <template>
-header
+    <header class="header">
+        <div class="header__title">
+            <img src="https://cdn.discordapp.com/attachments/1219948957048770610/1220980066511749160/sakazuki_logo_box_rgb.png?ex=6610e94c&is=65fe744c&hm=b0ddea06be9f2e8ec0fc725daed366242553b573a8181817d9f561e7d94a0a5a&">
+        </div>
+        <item class="navigation">
+            <ul>
+                <li><a href="/#">sakazukiとは？</a></li>
+                <li><a href="/#about">導入メリット</a></li>
+                <li><a href="/#infomation">モデル</a></li>
+                <li><a href="/#contact">コンタクト</a></li>
+            </ul>
+        </item>
+        <button
+        class="menu-button"
+        :class="{ open: menuOpen }"
+        @click="toggleMenu"
+        >
+        <span class="menu-button__bar"></span>
+        <span class="menu-button__bar"></span>
+        <span class="menu-button__bar"></span>
+        </button>
+        <transition name="fade">
+        <nav
+            class="nav"
+            :class="{ 'sp-menu': buttonShow }"
+            :style="{ right: menuOpen ? '0' : '-100%' }"
+        >
+            <ul class="nav__list">
+            <li class="nav__item">
+                <a href="/" class="nav__link nav__link--current">sakazukiとは？</a>
+            </li>
+            <li class="nav__item">
+                <a href="/about" class="nav__link">導入メリット</a>
+            </li>
+            <li class="nav__item">
+                <a href="/information" class="nav__link">モデル</a>
+            </li>
+            <li class="nav__item">
+                <a href="/contact" class="nav__link">コンタクト</a>
+            </li>
+            </ul>
+        </nav>
+        </transition>
+        <div v-show="menuOpen" class="overlay" @click="closeMenu"></div>
+    </header>
 </template>
 
 <script>
-
+export default {
+data() {
+    return {
+    menuOpen: false,
+    buttonShow: false,
+    screenWidth: window.innerWidth
+    };
+},
+methods: {
+    toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+    },
+    closeMenu() {
+    this.menuOpen = false;
+    },
+    handleResize() {
+    this.buttonShow = this.screenWidth < 768;
+    }
+},
+mounted() {
+    window.addEventListener("resize", this.handleResize);
+    this.handleResize();
+},
+beforeUnmounted() {
+    window.removeEventListener("resize", this.handleResize);
+}
+};
 </script>
 
-<style>
+<style scoped>
+.header {
+    position: fixed;
+    width: 98%;
+    padding: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #ffffff;
+    color: #000000;
+    box-shadow: 0px 1px 5px 0px gray;
+}
+
+.header__title {
+    margin: 0;
+    font-size: 1.5rem;
+}
+.header__title img {
+    width: 120px;
+}
+item ul {
+    list-style: none;
+    text-align: left;
+    display: flex;
+    margin: 0px;
+}
+
+item ul li {
+    display: inline;
+    margin-right: 20px;
+    margin-left: 10px;
+}
+
+item ul li a {
+    text-decoration: none;
+    font-size: 1rem;
+    font-weight: bold;
+    color: #333;
+}
+
+
+.navigation {
+    display: 1;
+}
+.menu-button {
+    appearance: none;
+    display: none;
+    flex-direction: column;
+    justify-content: space-between;
+    width: 40px;
+    height: 25px;
+    background: none;
+    border: none;
+    cursor: pointer;
+}
+
+.menu-button__bar {
+    width: 100%;
+    height: 3px;
+    background-color: #000000;
+    transition: transform 0.3s ease;
+}
+
+.menu-button.open .menu-button__bar:nth-child(1) {
+    transform: translateY(10px) rotate(45deg);
+}
+
+.menu-button.open .menu-button__bar:nth-child(2) {
+    opacity: 0;
+}
+
+.menu-button.open .menu-button__bar:nth-child(3) {
+    transform: translateY(-10px) rotate(-45deg);
+}
+
+.nav {
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    top: 60px;
+    width: 100%;
+    max-width: 300px;
+    height: calc(100vh - 60px);
+    background-color: #ffffff;
+    padding: 1rem;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    z-index: 10;
+    transition: right 0.3s ease;
+    right: -100%;
+}
+
+.nav__list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.nav__item {
+    margin-bottom: 30px;
+    margin-top: 10px;
+}
+
+.nav__link {
+    text-decoration: none;
+    color: #000000;
+    font-size: 1rem;
+}
+
+.nav__link:hover {
+    color: #333333;
+}
+
+.overlay {
+    display: none;
+}
+
+.sp-menu {
+    display: none;
+}
+
+@media (max-width: 768px) {
+    .header {
+        position: fixed;
+        width:97%;
+        padding: 5px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        background-color: #ffffff;
+        color: #000000;
+        box-shadow: 0px 1px 5px 0px gray;
+    }
+    .header__title {
+        margin: 0;
+        font-size: 1.5rem;
+    }
+    .menu-button {
+        display: flex;
+    }
+
+    .nav {
+        width: 30%;
+        position: fixed;
+        top: 54px;
+        right: 0;
+    }
+    .navigation{
+        display: none;
+    }
+
+    .overlay {
+        display: flex;
+        position: fixed;
+        top: 54px;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        background-color: rgba(0, 0, 0, 0.5);
+        z-index: 9;
+    }
+
+    .sp-menu {
+        display: block;
+        transition: 0.5s;
+    }
+
+    }
 </style>
